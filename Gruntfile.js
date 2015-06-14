f8a4f206181d245893007bcbe5aa9fc1f44d8157
Ramón Albertí <krainet@gmail.com>
2015-06-14 13:54:11 +0200

    Multitasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,35 @@
 var grunt = require('grunt');
 
-grunt.config.init({
-  ninja: {
-    movements: ['hadooken', 'sor-yukeen', 'k.o']
-  }
+grunt.initConfig({
+    multilog:{
+        task1:[1,2,3],
+        task2:['hi','im','ninja']
+    },
+    default:{},
+    ninja: {
+        movements: ['hadooken', 'sor-yukeen', 'k.o']
+    }
 });
 
 
 grunt.registerTask('world', 'world task description', function(){
-  console.log('hello world');
+    console.log('hello world');
 });
 
 grunt.registerTask('hello', 'say hello', function(name){
-  if(!name || !name.length)
-    grunt.warn('you need to provide a name.');
+    if(!name || !name.length)
+        grunt.warn('you need to provide a name.');
 
-  console.log('Hello ' + name);
-  console.log('Posible attacks:'+grunt.config.get('ninja').movements);
+    console.log('Hello ' + name);
+    console.log('Posible attacks:'+grunt.config.get('ninja').movements);
 });
 
+
 grunt.registerTask('default', ['world', 'hello:ninjas']);
+
+grunt.task.registerMultiTask('multilog', 'Log stuff.', function() {
+    //Ejecutamos todas las tareas de multilog con grunt
+    //Podemos ejecutar solo una task con grunt multilog:task1
+    grunt.log.writeln(this.target + ': ' + this.data);
+});
+
