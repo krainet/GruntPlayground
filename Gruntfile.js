41d39a3b1915e5ce51db9be5253d22e0f511998d
Ramón Albertí <krainet@gmail.com>
2015-06-14 13:17:07 +0200

    paso3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,12 @@
 var grunt = require('grunt');
 
+grunt.config.init({
+  ninja: {
+    movements: ['hadooken', 'sor-yukeen', 'k.o']
+  }
+});
+
+
 grunt.registerTask('world', 'world task description', function(){
   console.log('hello world');
 });
@@ -8,7 +15,8 @@ grunt.registerTask('hello', 'say hello', function(name){
   if(!name || !name.length)
     grunt.warn('you need to provide a name.');
 
-  console.log('hello ' + name);
+  console.log('Hello ' + name);
+  console.log('Posible attacks:'+grunt.config.get('ninja').movements);
 });
 
 grunt.registerTask('default', ['world', 'hello:ninjas']);
