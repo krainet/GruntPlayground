a56c7eabe51b3080c7519642c6006f4b9d585780
Ramón Albertí <krainet@gmail.com>
2015-06-14 13:06:42 +0200

    paso2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,14 @@
 var grunt = require('grunt');
 
-grunt.registerTask('default', 'default task description', function(){
+grunt.registerTask('world', 'world task description', function(){
   console.log('hello world');
 });
+
+grunt.registerTask('hello', 'say hello', function(name){
+  if(!name || !name.length)
+    grunt.warn('you need to provide a name.');
+
+  console.log('hello ' + name);
+});
+
+grunt.registerTask('default', ['world', 'hello:ninjas']);
